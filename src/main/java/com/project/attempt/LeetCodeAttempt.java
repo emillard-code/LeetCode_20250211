6626b2f563c1dffb8036030d5ff8f42c7efca2fb
Emerson Millard <emillard.code@gmail.com>
2025-02-13 10:22:14 -0800

    Added challenge attempt and solution logic.

--- src/main/java/com/project/attempt/LeetCodeAttempt.java
+++ src/main/java/com/project/attempt/LeetCodeAttempt.java
@@ -4,6 +4,27 @@ public class LeetCodeAttempt {
 
     public static void main(String[] args) {
 
+        System.out.println(removeAllOccurrencesOfASubstring("daabcbaabcbc", "abc"));
+        System.out.println(removeAllOccurrencesOfASubstring("axxxxyyyyb", "xy"));
+
+    }
+
+    public static String removeAllOccurrencesOfASubstring(String s, String part) {
+
+        while (s.contains(part)) {
+
+            for (int i = 0; i < s.length(); i++) {
+
+                if (s.startsWith(part, i)) {
+                    s = s.substring(0, i) + s.substring(i + part.length());
+                }
+
+            }
+
+        }
+
+        return s;
+
     }
 
 }
