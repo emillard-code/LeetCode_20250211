4577ef455a588035f73ef6a58ac0f1bfd28ae0a3
Emerson Millard <emillard.code@gmail.com>
2025-02-15 10:08:21 -0800

    Added comments to challenge attempt.

--- src/main/java/com/project/attempt/LeetCodeAttempt.java
+++ src/main/java/com/project/attempt/LeetCodeAttempt.java
@@ -9,12 +9,16 @@ public class LeetCodeAttempt {
 
     }
 
+    // This method removes all occurrences of a substring and returns the final result.
     public static String removeAllOccurrencesOfASubstring(String s, String part) {
 
+        // This while loop will continue as long as string s still contains the substring.
         while (s.contains(part)) {
 
+            // We loop through string s starting from the leftmost index.
             for (int i = 0; i < s.length(); i++) {
 
+                // Whenever we find an occurrence of the substring part, we remove it from string s.
                 if (s.startsWith(part, i)) {
                     s = s.substring(0, i) + s.substring(i + part.length());
                 }
@@ -23,6 +27,7 @@ public class LeetCodeAttempt {
 
         }
 
+        // Once we have exited the while loop, we then return string s.
         return s;
 
     }
